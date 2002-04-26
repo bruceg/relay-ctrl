1039a5a9f0da0927c6dd52d35666f223cfe752f3
Bruce Guenter <bruce@untroubled.org>
2002-04-26 21:42:50 +0000

    Modified to send the acknowledgement packet only if the file could be created.

--- relay-ctrl-udp.c
+++ relay-ctrl-udp.c
@@ -37,9 +37,9 @@ int main(void)
       buffer[len] = 0;
       if ((ip = validate_ip(buffer)) == 0)
 	warn3("Invalid IP from '", ipv4_format(addr), "'.");
+      else if (!touch(ip))
+	warn3sys("Could not touch '", ip, "'");
       else {
-	if (!touch(ip))
-	  warn3sys("Could not touch '", ip, "'");
 	buffer[0] = 1;
 	socket_send4(sock, buffer, 1, addr, port);
       }
